b1dd7aef5796a694469e0dc84887bf3778c8aac6
Margaret Scott <mscott@visiblemeasures.com>
2013-10-11 10:25:57 -0400

    Git, please don't store my WordPress password. Thanks.  https://github.com/eudaimonious/gravastate/blob/master/lolcommits/e797b9db25c.gif

--- gravatir.rb
+++ gravatir.rb
@@ -1,5 +1,10 @@
 require 'watir-webdriver'
+require File.join(File.dirname(__FILE__), 'password')
 
 browser = Watir::Browser.new :firefox
 browser.goto "https://en.gravatar.com/"
+
+#Logging In to Gravatar
 browser.link(:text => 'Sign In').click
+browser.text_field(:id, "user_login").set("eudaimonious2013")
+browser.text_field(:id, "user_pass").set(@password)
